84ebe527376e0882305bacfa87f45b608634334e
Ryan Born <ryanrborn@gmail.com>
2015-06-18 09:23:28 -0600

    fixed view url's

--- webroot/js/config/Routes.js
+++ webroot/js/config/Routes.js
@@ -5,27 +5,27 @@
 		.factory('Routes', [function RoutesFactory() {
 			var routes = {
 				"/": {
-					templateUrl: "./src/page/view/landing.html",
+					templateUrl: "/js/page/view/landing.html",
 					controller: "LandingController",
 					requiredLogin: 0
 				},
 				"/sign-in": {
-					templateUrl: "./src/auth/view/auth.html",
+					templateUrl: "/js/auth/view/auth.html",
 					controller: "AuthController",
 					requiredLogin: 0
 				},
 				"/logout": {
-					templateUrl: "./src/auth/view/auth.html",
+					templateUrl: "/js/auth/view/auth.html",
 					controller: "AuthController",
 					requiredLogin: 1
 				},
 				"/users": {
-					templateUrl: "./src/users/view/index.html",
+					templateUrl: "/js/users/view/index.html",
 					controller: "UserListController",
 					requiredLogin: 0
 				},
 				"/users/:id": {
-					templateUrl: "./src/users/view/user.html",
+					templateUrl: "/js/users/view/user.html",
 					controller: "UserDetailController",
 					requiredLogin: 0
 				},
