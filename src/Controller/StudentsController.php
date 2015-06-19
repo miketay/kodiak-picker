e3e550d9e97ad5f6ff1f1660536e40369bc5ab43
Ryan Born <ryanrborn@gmail.com>
2015-06-19 11:38:40 -0600

    student import working on backend

--- src/Controller/StudentsController.php
+++ src/Controller/StudentsController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use App\Controller\AppController;
+use Cake\ORM\TableRegistry;
 
 /**
  * Students Controller
@@ -24,7 +25,7 @@ class StudentsController extends AppController
      */
     public function index()
     {
-        $this->set('students', $this->paginate($this->Students));
+        $this->set('students', $this->Students->find('all'));
         $this->set('_serialize', ['students']);
     }
 
@@ -117,35 +118,42 @@ class StudentsController extends AppController
 	{
 		// get file and parse it into db
 		$file = $this->request->input();
-		$listStart = strpos($file, "last_name");
+		$listStart = strpos($file, "last_name\tfirst_name\tgrade_level\tStudent_Number");
+		if ($listStart === false) {
+			throw new \Cake\Network\Exception\BadRequestException("Malformed File");
+		}
 		$file = substr($file, $listStart);
 		$file = explode("\r\n", $file);
 
-		$headers = explode("\t", $file[0]);
-		$students = [];
-		$rows = count($file);
-		for ($i = 1; $i<$rows; $i++) {
-			$row = explode("\t", $file[$i]);
-			if (count($row) <= 1) {
-				break;
+		$StudentTable = TableRegistry::get('Students');
+		$StudentTable->connection()->transactional(function() use ($StudentTable, $file) {
+			$rows = count($file);
+			$headers = explode("\t", $file[0]);
+			for ($i = 1; $i<$rows; $i++) {
+				$row = explode("\t", $file[$i]);
+				if (count($row) <= 1) {
+					break;
+				}
+				$num = count($row);
+				$student = [];
+				for ($j = 0; $j<$num; $j++) {
+					$header = $headers[$j] == "Student_Number" ? "id" : $headers[$j];
+					$student[$header] = is_numeric($row[$j]) ? intval($row[$j]) : $row[$j];
+				}
+				// cake 3 doesn't have a saveAll that checks for existing records
+				// so we have to do it indiviually
+				$student = $StudentTable->newEntity($student, [
+					'accessibleFields' => ['id' => true]
+				]);
+				if (!$StudentTable->save($student)) {
+					throw new \Cake\Network\Exception\InternalErrorException("Couldn't save student {$student['id']}");
+				}
 			}
-			$num = count($row);
-			$student = [];
-			for ($j = 0; $j<$num; $j++) {
-				$header = $headers[$j] == "Student_Number" ? "id" : $headers[$j];
-				$student[$header] = is_numeric($row[$j]) ? intval($row[$j]) : $row[$j];
-			}
-			$students[] = $student;
-		}
-
-		// TODO: put students into database, this doesn't seem to work...
-		$result = $this->Students->save($students);
-				
+		});
 
 		$this->set([
-			'result' => $result,
 			'message' => "Success",
-			'_serialize' => ['result','message']
+			'_serialize' => ['message']
 		]);
 	}
 }
