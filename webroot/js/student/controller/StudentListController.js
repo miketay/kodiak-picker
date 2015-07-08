bbcf553a9015f70f22e31e983ea4ec5ec7b7079b
Ryan Born <ryanb@gurutechnologies.net>
2015-07-08 12:18:35 -0600

    added ability to delete student closes #7

--- webroot/js/student/controller/StudentListController.js
+++ webroot/js/student/controller/StudentListController.js
@@ -2,10 +2,28 @@
 	'use strict';
 
 	angular.module('Student')
-		.controller('StudentListController', ['$scope', '$location', 'Page', 'StudentResource', function($scope, $location, Page, StudentResource) {
+		.controller('StudentListController', ['$scope', '$location', '$mdDialog', 'Page', 'StudentResource', function($scope, $location, $mdDialog, Page, StudentResource) {
 			Page.title('Student List');
 
 			$scope.students = StudentResource.query();
+
+			$scope.delete = function(index, ev) {
+				var confirm = $mdDialog.confirm()
+					.parent(angular.element(document.body))
+					.title("Are you sure")
+					.content("Delete "+$scope.students[index].first_name+"?")
+					.ariaLabel("Confirmation")
+					.ok("Delete")
+					.cancel("Cancel")
+					.targetEvent(ev);
+				$mdDialog.show(confirm).then(function() {
+					$scope.students[index].$delete(function() {
+						$scope.students.splice(index, 1);
+					});
+				}, function() {
+					// do nufin
+				});
+			};
 
 		}]);
 })();
