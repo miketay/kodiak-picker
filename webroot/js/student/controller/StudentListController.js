9bb8e9bfc90466e9ef0251c1f8bd61bb050278ec
Ryan Born <ryanb@gurutechnologies.net>
2015-09-16 15:01:58 -0600

    added delete all button, added confirmation to delete 8th graders closes #23

--- webroot/js/student/controller/StudentListController.js
+++ webroot/js/student/controller/StudentListController.js
@@ -29,14 +29,14 @@
 				});
 			};
 
-			$scope.delete8 = function() {
+			$scope.delete8 = function(ev) {
 				$scope.deleting = true;
 				var delete8 = function(i) {
 					if (i < 0) {
+						$scope.deleting = false;
 						if (!$scope.$$phase) {
 							$scope.$digest();
 						}
-						$scope.deleting = false;
 						return;
 					}
 					if ($scope.students[i].grade_level == 8) {
@@ -49,7 +49,52 @@
 					}
 				};
 				var i = $scope.students.length-1;
-				delete8(i);
+				var confirm = $mdDialog.confirm()
+					.parent(angular.element(document.body))
+					.title("Are you sure")
+					.content("Delete all 8th graders?")
+					.ariaLabel("Confirmation")
+					.ok("Delete")
+					.cancel("Cancel")
+					.targetEvent(ev);
+				$mdDialog.show(confirm).then(function() {
+					var i = $scope.students.length-1;
+					delete8(i);
+				}, function() {
+					// do nufin
+				});
+			};
+
+			$scope.deleteAll = function(ev) {
+				$scope.deleting = true;
+				var deleteAll = function(i) {
+					if (i < 0) {
+						$scope.deleting = false;
+						if (!$scope.$$phase) {
+							$scope.$digest();
+						}
+						return;
+					}
+					$scope.students[i].$delete(function() {
+						$scope.students.splice(i, 1);
+						deleteAll(i-1);
+					});
+				};
+				var confirm = $mdDialog.confirm()
+					.parent(angular.element(document.body))
+					.title("Are you sure")
+					.content("Delete all students?")
+					.ariaLabel("Confirmation")
+					.ok("Delete")
+					.cancel("Cancel")
+					.targetEvent(ev);
+				$mdDialog.show(confirm).then(function() {
+					var i = $scope.students.length-1;
+					deleteAll(i);
+				}, function() {
+					// do nufin
+				});
+
 			};
 
 		}]);
